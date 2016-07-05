23d3b3b0d3e0b309559fb7ba15d8a86a210fe477
Matt Gershowitz <mattgershowitz1@gmail.com>
2016-07-05 09:24:18 -0400

    added conditional to quote length

--- public/js/script.js
+++ public/js/script.js
@@ -60,7 +60,7 @@ const randomQuote = ()=>{
     method: "GET",
     dataType: 'json',
     success: (data)=>{
-      if(data.quote.length < 200) {
+      if(data.quote.length > 200) {
         $('#desc').append($('<li>').attr('class', 'quote').text('"'+data.quote.substr(0,200)+'..."'))
       } else {
         $('#desc').append($('<li>').attr('class', 'quote').text('"'+data.quote+'"'))
